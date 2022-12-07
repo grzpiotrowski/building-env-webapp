5ce7924b450d9d818f78af6851a0a0fbe2cc818b
Grzegorz Piotrowski <geopiotrowski@gmail.com>
2022-12-07 20:19:30 +0000

    Updated 3D model and scene lighting

--- public/js/viewer.js
+++ public/js/viewer.js
@@ -4,23 +4,17 @@ import { OrbitControls} from 'three/addons/controls/OrbitControls.js';
 
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer();
+renderer.setClearColor(0x6d7372, 1);
 
 let container = document.getElementById('viewer-container');
 renderer.setSize($(container).width(), $(container).height());
 container.appendChild(renderer.domElement);
 
 // Orbit camera controls
-const controls = new OrbitControls( camera, renderer.domElement );
-
-// Example cube object
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-cube.translateY(5);
-scene.add( cube );
+const controls = new OrbitControls(camera, renderer.domElement);
 
 // Instantiate a loader
 const loader = new GLTFLoader();
@@ -30,8 +24,8 @@ loader.load(
 	// resource URL
 	'models/building-model.glb',
 	// called when the resource is loaded
-	function ( gltf ) {
-		scene.add( gltf.scene );
+	function (gltf) {
+		scene.add(gltf.scene);
 
 		gltf.animations; // Array<THREE.AnimationClip>
 		gltf.scene; // THREE.Group
@@ -40,11 +34,11 @@ loader.load(
 		gltf.asset; // Object
 	},
 	// called while loading is progressing
-	function ( xhr ) {
-		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+	function (xhr) {
+		console.log((xhr.loaded / xhr.total * 100) + '% loaded');
 	},
 	// called when loading has errors
-	function ( error ) {
+	function (error) {
 		console.log( 'An error happened' );
 	}
 );
@@ -55,16 +49,22 @@ camera.position.z = 3.86;
 //controls.update() must be called after any manual changes to the camera's transform
 controls.update(); 
 
-const directionalLight = new THREE.DirectionalLight( 0xffffff, 1 );
-scene.add( directionalLight );
+const directionalLight = new THREE.DirectionalLight( 0xffffff, 3 );
+directionalLight.translateX(15);
+directionalLight.translateZ(10);
+scene.add(directionalLight);
+
+const directionalLight2 = new THREE.DirectionalLight( 0xffffff, 1 );
+directionalLight2.translateX(-15);
+directionalLight2.translateZ(-10);
+scene.add(directionalLight2);
+
+const directionalLightTopDown = new THREE.DirectionalLight( 0xffffff, 2 );
+scene.add(directionalLightTopDown);
 
 function animate() {
-    requestAnimationFrame( animate );
-
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-
-    renderer.render( scene, camera );
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
 };
 
 animate();
